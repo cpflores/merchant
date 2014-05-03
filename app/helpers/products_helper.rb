014a3878190c8c77957ee98d288f76c4ac2d2444
Chadwick Flores <chadwickflores@gmail.com>
2014-05-03 14:40:04 -0500

    update products index

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,12 @@
 module ProductsHelper
+	
+	def print_price(price)
+		number_to_currency price
+	end
+
+	def price=(input)
+		input.delete!("$")
+		super
+	end
+
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ActiveRecord::Base
+
+	validates_numericality_of :price
+
 end
